47b0432f392d11cb90ec7cdcfffb9b39efdbf15c
Qk4req <qk4req@gmail.com>
2020-12-02 22:12:45 +0400

    updated database structure

--- app.sql
+++ app.sql
@@ -1,6 +1,6 @@
 -- --------------------------------------------------------
 -- Хост:                         127.0.0.1
--- Версия сервера:               8.0.19 - MySQL Community Server - GPL
+-- Версия сервера:               5.7.29-log - MySQL Community Server (GPL)
 -- Операционная система:         Win64
 -- HeidiSQL Версия:              11.0.0.5958
 -- --------------------------------------------------------
@@ -14,20 +14,20 @@
 
 -- Дамп структуры базы данных qk4req
 DROP DATABASE IF EXISTS `qk4req`;
-CREATE DATABASE IF NOT EXISTS `qk4req` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE IF NOT EXISTS `qk4req` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
 USE `qk4req`;
 
 -- Дамп структуры для таблица qk4req.admins
 DROP TABLE IF EXISTS `admins`;
 CREATE TABLE IF NOT EXISTS `admins` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `signature` varchar(50) NOT NULL,
-  `level` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `signature` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `level` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `signature` (`signature`),
   KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Дамп данных таблицы qk4req.admins: ~0 rows (приблизительно)
 DELETE FROM `admins`;
@@ -39,15 +39,15 @@ INSERT INTO `admins` (`id`, `name`, `signature`, `level`) VALUES
 -- Дамп структуры для таблица qk4req.challenges
 DROP TABLE IF EXISTS `challenges`;
 CREATE TABLE IF NOT EXISTS `challenges` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(50) NOT NULL,
-  `description` varchar(1000) NOT NULL,
-  `link` varchar(500) DEFAULT NULL,
-  `start` int NOT NULL,
-  `end` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `link` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `start` int(11) NOT NULL,
+  `end` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `title` (`title`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Дамп данных таблицы qk4req.challenges: ~2 rows (приблизительно)
 DELETE FROM `challenges`;
@@ -60,24 +60,24 @@ INSERT INTO `challenges` (`id`, `title`, `description`, `link`, `start`, `end`) 
 -- Дамп структуры для таблица qk4req.donations
 DROP TABLE IF EXISTS `donations`;
 CREATE TABLE IF NOT EXISTS `donations` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `notification_id` int DEFAULT NULL,
-  `easter_egg_id` int DEFAULT NULL,
-  `from` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `notification_id` int(11) DEFAULT NULL,
+  `easter_egg_id` int(11) DEFAULT NULL,
+  `from` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `amount` float(11,2) unsigned DEFAULT NULL,
   `original_amount` float(11,4) unsigned DEFAULT NULL,
-  `original_currency` char(5) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `comment` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `original_currency` char(5) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `comment` varchar(1000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` datetime NOT NULL,
-  `platform` enum('da','donationalerts','sl','streamlabs') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `dubbing_src` varchar(1000) DEFAULT NULL,
-  `status` enum('shown','hidden') CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `platform` enum('da','donationalerts','sl','streamlabs') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dubbing_src` varchar(1000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` enum('shown','hidden') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK1` (`notification_id`),
   KEY `FK2` (`easter_egg_id`),
   CONSTRAINT `FK_to_easter_eggs1` FOREIGN KEY (`easter_egg_id`) REFERENCES `easter_eggs` (`id`),
   CONSTRAINT `FK_to_notification1` FOREIGN KEY (`notification_id`) REFERENCES `notifications` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=88 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Дамп данных таблицы qk4req.donations: ~0 rows (приблизительно)
 DELETE FROM `donations`;
@@ -87,11 +87,11 @@ DELETE FROM `donations`;
 -- Дамп структуры для таблица qk4req.dubbings
 DROP TABLE IF EXISTS `dubbings`;
 CREATE TABLE IF NOT EXISTS `dubbings` (
-  `id` int NOT NULL,
-  `src` varchar(1000) NOT NULL,
-  `volume` tinyint NOT NULL,
+  `id` int(11) NOT NULL,
+  `src` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `volume` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Дамп данных таблицы qk4req.dubbings: ~0 rows (приблизительно)
 DELETE FROM `dubbings`;
@@ -101,77 +101,83 @@ DELETE FROM `dubbings`;
 -- Дамп структуры для таблица qk4req.easter_eggs
 DROP TABLE IF EXISTS `easter_eggs`;
 CREATE TABLE IF NOT EXISTS `easter_eggs` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `expression` varchar(10) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `expression` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
   `value` float(11,2) NOT NULL,
-  `src` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `src` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Дамп данных таблицы qk4req.easter_eggs: ~1 rows (приблизительно)
+-- Дамп данных таблицы qk4req.easter_eggs: ~7 rows (приблизительно)
 DELETE FROM `easter_eggs`;
 /*!40000 ALTER TABLE `easter_eggs` DISABLE KEYS */;
 INSERT INTO `easter_eggs` (`id`, `expression`, `value`, `src`) VALUES
-	(2, '>=', 1000.00, '/assets/media/easter_eggs/dimooon.mp3');
+	(2, '>=', 100.00, '/assets/media/easter_eggs/bratishka.mp3'),
+	(3, '>=', 200.00, '/assets/media/easter_eggs/88005553535.mp3'),
+	(4, '>=', 500.00, '/assets/media/easter_eggs/dimooon.mp3'),
+	(5, '>=', 1000.00, '/assets/media/easter_eggs/uebishe.mp3'),
+	(6, '>=', 5000.00, '/assets/media/easter_eggs/pfrf.mp3'),
+	(7, '>=', 10000.00, '/assets/media/easter_eggs/akulenok.mp3'),
+	(9, '>=', 50000.00, '/assets/media/easter_eggs/kari4.mp3');
 /*!40000 ALTER TABLE `easter_eggs` ENABLE KEYS */;
 
 -- Дамп структуры для таблица qk4req.followers
 DROP TABLE IF EXISTS `followers`;
 CREATE TABLE IF NOT EXISTS `followers` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `notification_id` int NOT NULL,
-  `name` varchar(50) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `notification_id` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` datetime NOT NULL,
-  `status` enum('shown','hidden') DEFAULT NULL,
+  `platform` enum('tw','twitch','sl','streamlabs') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'streamlabs',
+  `status` enum('shown','hidden') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
   KEY `FK1` (`notification_id`),
   CONSTRAINT `FK_to_notification2` FOREIGN KEY (`notification_id`) REFERENCES `notifications` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Дамп данных таблицы qk4req.followers: ~0 rows (приблизительно)
 DELETE FROM `followers`;
 /*!40000 ALTER TABLE `followers` DISABLE KEYS */;
-INSERT INTO `followers` (`id`, `notification_id`, `name`, `created_at`, `status`) VALUES
-	(1, 6, 'test1', '2020-09-26 22:45:25', 'shown'),
-	(2, 6, 'test2', '2020-09-26 22:45:25', 'shown');
+INSERT INTO `followers` (`id`, `notification_id`, `name`, `created_at`, `platform`, `status`) VALUES
+	(3, 6, 'udm_tv', '2020-12-02 17:06:06', 'streamlabs', 'shown');
 /*!40000 ALTER TABLE `followers` ENABLE KEYS */;
 
 -- Дамп структуры для таблица qk4req.notifications
 DROP TABLE IF EXISTS `notifications`;
 CREATE TABLE IF NOT EXISTS `notifications` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `type` enum('donation','follower','subscription','progress_bar','dubbing') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `src` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` enum('donation','follower','subscription','progress_bar','dubbing') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `src` varchar(1000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `type` (`type`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Дамп данных таблицы qk4req.notifications: ~5 rows (приблизительно)
+-- Дамп данных таблицы qk4req.notifications: ~4 rows (приблизительно)
 DELETE FROM `notifications`;
 /*!40000 ALTER TABLE `notifications` DISABLE KEYS */;
 INSERT INTO `notifications` (`id`, `type`, `src`) VALUES
-	(4, 'donation', '/assets/media/notifications/donation.mp3'),
-	(6, 'follower', '/assets/media/notifications/follower.mp3'),
-	(7, 'subscription', '/assets/media/notifications/subscription.mp3'),
-	(8, 'progress_bar', '/assets/media/notifications/progress_bar.mp3'),
-	(9, 'dubbing', NULL);
+	(4, 'donation', '/assets/media/notifications/shnir-don.mp3'),
+	(6, 'follower', '/assets/media/notifications/zdrastitya-fol.mp3'),
+	(7, 'subscription', '/assets/media/notifications/dimooon-sub.mp3'),
+	(8, 'progress_bar', '/assets/media/notifications/progress_bar.mp3');
 /*!40000 ALTER TABLE `notifications` ENABLE KEYS */;
 
 -- Дамп структуры для таблица qk4req.progress_bar_levels
 DROP TABLE IF EXISTS `progress_bar_levels`;
 CREATE TABLE IF NOT EXISTS `progress_bar_levels` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `session_id` int NOT NULL,
-  `notification_id` int NOT NULL,
-  `title` varchar(50) NOT NULL DEFAULT '0',
-  `points` int NOT NULL,
-  `color` varchar(10) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `session_id` int(11) NOT NULL,
+  `notification_id` int(11) NOT NULL,
+  `title` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
+  `points` int(11) NOT NULL,
+  `color` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `notification_id` (`notification_id`),
   KEY `stream_id` (`session_id`),
   CONSTRAINT `FK_to_notification4` FOREIGN KEY (`notification_id`) REFERENCES `notifications` (`id`),
   CONSTRAINT `FK_to_session1` FOREIGN KEY (`session_id`) REFERENCES `sessions` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Дамп данных таблицы qk4req.progress_bar_levels: ~5 rows (приблизительно)
 DELETE FROM `progress_bar_levels`;
@@ -187,48 +193,33 @@ INSERT INTO `progress_bar_levels` (`id`, `session_id`, `notification_id`, `title
 -- Дамп структуры для таблица qk4req.progress_bar_points
 DROP TABLE IF EXISTS `progress_bar_points`;
 CREATE TABLE IF NOT EXISTS `progress_bar_points` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `session_id` int NOT NULL,
-  `amount` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `session_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `session_id` (`session_id`),
   CONSTRAINT `FK_to_session2` FOREIGN KEY (`session_id`) REFERENCES `sessions` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Дамп данных таблицы qk4req.progress_bar_points: ~0 rows (приблизительно)
 DELETE FROM `progress_bar_points`;
 /*!40000 ALTER TABLE `progress_bar_points` DISABLE KEYS */;
 /*!40000 ALTER TABLE `progress_bar_points` ENABLE KEYS */;
 
--- Дамп структуры для таблица qk4req.sessions
-DROP TABLE IF EXISTS `sessions`;
-CREATE TABLE IF NOT EXISTS `sessions` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `start` int NOT NULL,
-  `end` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
--- Дамп данных таблицы qk4req.sessions: ~0 rows (приблизительно)
-DELETE FROM `sessions`;
-/*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
-INSERT INTO `sessions` (`id`, `start`, `end`) VALUES
-	(1, 1400939566, 0);
-/*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
-
 -- Дамп структуры для таблица qk4req.subscriptions
 DROP TABLE IF EXISTS `subscriptions`;
 CREATE TABLE IF NOT EXISTS `subscriptions` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `notification_id` int NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `months` int NOT NULL DEFAULT '1',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `notification_id` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `months` int(11) NOT NULL DEFAULT '1',
   `created_at` datetime NOT NULL,
-  `status` enum('shown','hidden') DEFAULT NULL,
+  `status` enum('shown','hidden') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
   KEY `FK1` (`notification_id`),
   CONSTRAINT `FK_to_notification3` FOREIGN KEY (`notification_id`) REFERENCES `notifications` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Дамп данных таблицы qk4req.subscriptions: ~0 rows (приблизительно)
 DELETE FROM `subscriptions`;
@@ -238,16 +229,16 @@ DELETE FROM `subscriptions`;
 -- Дамп структуры для таблица qk4req.vote
 DROP TABLE IF EXISTS `vote`;
 CREATE TABLE IF NOT EXISTS `vote` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `voting_id` int NOT NULL,
-  `voting_point_id` int NOT NULL,
-  `signature` varchar(50) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `voting_id` int(11) NOT NULL,
+  `voting_point_id` int(11) NOT NULL,
+  `signature` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK1` (`voting_point_id`),
   KEY `FK2` (`voting_id`),
   CONSTRAINT `FK_to_voting1` FOREIGN KEY (`voting_id`) REFERENCES `votings` (`id`),
   CONSTRAINT `FK_to_voting_point` FOREIGN KEY (`voting_point_id`) REFERENCES `voting_points` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Дамп данных таблицы qk4req.vote: ~2 rows (приблизительно)
 DELETE FROM `vote`;
@@ -260,13 +251,13 @@ INSERT INTO `vote` (`id`, `voting_id`, `voting_point_id`, `signature`) VALUES
 -- Дамп структуры для таблица qk4req.votings
 DROP TABLE IF EXISTS `votings`;
 CREATE TABLE IF NOT EXISTS `votings` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(50) NOT NULL,
-  `start` int NOT NULL,
-  `end` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `start` int(11) NOT NULL,
+  `end` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `title` (`title`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Дамп данных таблицы qk4req.votings: ~2 rows (приблизительно)
 DELETE FROM `votings`;
@@ -279,15 +270,15 @@ INSERT INTO `votings` (`id`, `title`, `start`, `end`) VALUES
 -- Дамп структуры для таблица qk4req.voting_points
 DROP TABLE IF EXISTS `voting_points`;
 CREATE TABLE IF NOT EXISTS `voting_points` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `voting_id` int NOT NULL,
-  `title` varchar(50) NOT NULL,
-  `background_color` varchar(50) NOT NULL,
-  `border_color` varchar(50) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `voting_id` int(11) NOT NULL,
+  `title` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `background_color` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `border_color` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK1` (`voting_id`),
   CONSTRAINT `FK_to_voting2` FOREIGN KEY (`voting_id`) REFERENCES `votings` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Дамп данных таблицы qk4req.voting_points: ~8 rows (приблизительно)
 DELETE FROM `voting_points`;
